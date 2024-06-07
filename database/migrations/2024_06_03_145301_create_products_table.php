faeb43658027cd7619a37b8eb38a316005736ca6
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-07 15:17:17 +0200

    feat: add migrations

--- database/migrations/2024_06_03_145301_create_products_table.php
+++ database/migrations/2024_06_03_145301_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
             $table->string('name');
             $table->text('description')->nullable();
             $table->double('price', 6,2);
--- database/seeders/CartsTableSeeder.php
+++ database/seeders/CartsTableSeeder.php
@@ -2,14 +2,14 @@
 
 namespace Database\Seeders;
 
-
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-class ProductsTableSeeder extends Seeder
+
+class CartsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -20,10 +20,10 @@ class ProductsTableSeeder extends Seeder
     {
         for($i = 0; $i < 10; $i++){
             $newProject = new Product();
-            $newProject->name = $faker->word();
-            $newProject->image = 'https://picsum.photos/200/300';
-            $newProject->description = $faker->sentence();
-            $newProject->price = $faker->randomNumber(3); 
+            $newProject->user_id = $faker->randomNumber(3);
+            $newProject->product_id = $faker->randomNumber(3);
+            $newProject->quntity = $faker->randomNumber(3);
+            $newProject->total = $faker->randomNumber(3); 
             $newProject->quantity = $faker->randomNumber(4);
             $newProject->category = $faker->word();
             $newProject->save();
